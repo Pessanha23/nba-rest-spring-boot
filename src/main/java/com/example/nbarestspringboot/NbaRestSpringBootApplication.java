2778377c442ba0b5ccec07ac7e142c3ebd16c34c
João Ricardo Pagano Pessanha <joao.ricardopagano@gmail.com>
2023-04-26 19:23:53 -0300

    Initial commit

---
+++ src/main/java/com/example/nbarestspringboot/NbaRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbarestspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbarestspringboot/NbaRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbarestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
